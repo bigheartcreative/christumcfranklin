40387e9480911902e2389677fc689be0efe9788b
bigheartcreative <erin@bigheartcreative.com>
2015-06-08 21:52:44 -0500

    Auto-adjust height in feature 3-up blocks

--- wp-content/themes/organic_natural/js/jquery.custom.js
+++ wp-content/themes/organic_natural/js/jquery.custom.js
@@ -81,6 +81,42 @@
 		$('.feature-vid, .postarea').fitVids();
 		
 	}
+
+	/* Auto adjust height of feature container divs  ---------------------*/	
+	equalheight = function(container) {
+
+		var currentTallest = 0,
+		    currentRowStart = 0,
+		    rowDivs = new Array(),
+		    $el,
+		    topPosition = 0;
+		$(container).each(function() {
+		
+		    $el = $(this);
+		    $($el).height('auto')
+		    topPostion = $el.position().top;
+		
+		    if (currentRowStart != topPostion) {
+		      for (currentDiv = 0; currentDiv < rowDivs.length; currentDiv++) {
+		        rowDivs[currentDiv].height(currentTallest);
+		      }
+		      
+				rowDivs.length = 0; // empty the array
+			    currentRowStart = topPostion;
+			    currentTallest = $el.height();
+			     rowDivs.push($el);
+			} else {
+		      rowDivs.push($el);
+		      currentTallest = (currentTallest < $el.height()) ? ($el.height()) : (currentTallest);
+		    }
+		    
+		    for (currentDiv = 0; currentDiv < rowDivs.length; currentDiv++) {
+		      rowDivs[currentDiv].height(currentTallest);
+		    }
+		  
+		});
+	}
+	
 	
 	$( document )
 	.ready( removeNoJsClass )
@@ -92,7 +128,9 @@
 	$( window )
 	.load( flexSliderSetup )
 	.load( isotopeSetup )
+	.load( function() {equalheight('.featured-pages .holder .information') })
 	.resize( sizingSetup )
-	.resize( isotopeSetup );
+	.resize( isotopeSetup )
+	.resize( function() {equalheight('.featured-pages .holder .information') });
 	
 })( jQuery );
