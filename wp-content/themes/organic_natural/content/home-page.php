48992697ed0efaf0e48aaa87683fa2d6baf54987
bigheartcreative <erin@bigheartcreative.com>
2015-06-16 21:24:42 -0500

    adjustment to homepage feature containers

--- wp-content/themes/organic_natural/content/home-page.php
+++ wp-content/themes/organic_natural/content/home-page.php
@@ -14,13 +14,13 @@
 			<h2 class="title text-center"><?php the_title(); ?></h2>
 			<?php the_excerpt(); ?>
 		</div>
-		
-		<div class="align-center text-center">
-			<a class="button" href="<?php the_permalink(); ?>" rel="bookmark" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'organicthemes' ), the_title_attribute( 'echo=0' ) ) ); ?>"><?php _e("Learn More", 'organicthemes'); ?></a>
-		</div>
-	
+			
 	<!-- END .article -->
 	</div>
 
 <!-- END .information -->
+</div>
+
+<div class="learn-more align-center text-center">
+	<a class="button" href="<?php the_permalink(); ?>" rel="bookmark" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'organicthemes' ), the_title_attribute( 'echo=0' ) ) ); ?>"><?php _e("Learn More", 'organicthemes'); ?></a>
 </div>
